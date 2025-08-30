a6d91c807c73b1c7527ad6b86e0de8823f369f0b
KavaliVandana <kavalivandana2601@gmail.com>
2025-08-30 16:59:57 +0530

    changes done changes done

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/PROJECT",
+  base: "/project/",
   
   plugins: [
     react(),
